01eb9ce6db68f2e49cccd562f34524d3727ec124
Rattlehead90 <strogonov@seznam.cz>
2022-12-11 17:30:21 +0100

    Add validations to forms

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,5 +1,6 @@
 class Kitten < ApplicationRecord
   validates :name, :age, :cuteness, :softness, presence: true
-  validates :cuteness, length: { in: 1..10 }
-  validates :softness, length: { in: 1..10 }
+  validates :cuteness, numericality: { in: 1..10 }
+  validates :softness, numericality: { in: 1..10 }
+  validates :age, numericality: { in: 0..5 }
 end
